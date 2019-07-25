c73a8a8d25c32d5ea6549b850363f949f0a76542
dvento <dan@pop-os.localdomain>
2019-07-25 14:57:36 +0200

    Final solution. Two methods to calculate the number. I'm not sure about the my usage of "int()", I think it's repetitive and I'm sure there's a more efficient way to cast the calculations and the result to ints

--- 1/1.py
+++ 1/1.py
@@ -1,1 +1,24 @@
 # hey
+
+sumVal = 0
+
+def calculateLinear():
+    currNum = 0
+    global sumVal
+
+    while currNum < 1000:
+
+        if currNum % 3  == 0 or currNum % 5 == 0:
+            sumVal = sumVal + currNum
+        
+        currNum = currNum + 1
+
+def calculateGeometric(div,maxim):
+    
+    return int(div*(int(maxim/div)*(int((maxim/div))+1))/2)
+
+def getAndPrintResults():
+    sumVal = calculateGeometric(5,999) + calculateGeometric(3,999) - calculateGeometric(15,999)
+    print(f'{"The sum of all multiples of 3 and 5 that are less than 1000 is: "}{sumVal}')
+
+getAndPrintResults()
