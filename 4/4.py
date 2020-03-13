b42c07e949162d098598327994824b34ddccebe3
Dan Vento <dvento.fin@gmail.com>
2020-03-13 12:53:10 +0100

    Second version (still slow)

--- 4/4.py
+++ 4/4.py
@@ -11,12 +11,11 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
 
-# first version of 4
-
-
+# first version of problem 4
 
 def iterate():
 
+    # initialize variables
     x, y = 999, 999
     factorState, result = 0, 0
 
@@ -29,59 +28,42 @@ def iterate():
         result = x * y
 
         if isPalindromic(result):
+            # compare last found palindromic to the new one, so it keeps the higher
             if result > lastPalNum:
                 lastPalNum = result
 
+        # first, multiply 999 by 999, 998, 997... until 100
         if factorState == 1:
             y-=1
             if y < 100:
                 factorState = 0
-                y = 999
+        # then, decrease x by one and multiply it by the rest of digits
         else:
-            x-=1
+            x -= 1
+            y = x
             factorState = 1
 
         if x < 100:
             print("Palindromic nums: ", lastPalNum)
             break
 
-
-    '''
-    while True:
-
-        
-        result = x * y
-        print("Result: %i X: %i Y: %i" % (result, x, y))
-
-        if isPalindromic(result):
-            print("Is palindromic: %i" %(result))
-            break
-
-        if factorState == 1:
-            x-=1
-            factorState = 0
-        elif factorState == 0:
-            y-=1
-            factorState = 1
-
-        if x < 100 or y < 100:
-            break
-'''
-
 def isPalindromic(res):
 
+    # convert number into string  so we can compare its digits
     resArr = [int(x) for x in str(res)]
     resLen = len(resArr)
 
     left = resArr[0]
     right = resArr[resLen - 1]
+    # trick to find the number of times we have to iterate through the number
     halfLen = resLen / 2
-
 
     i = 0
 
+    # compare each digit starting from the first 
     while halfLen > 0:
 
+        # if the first digit does not equal the last one, just skip this number (call return)
         if left != right:
             return False
         else:
